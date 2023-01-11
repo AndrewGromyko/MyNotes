d8b09c73d183ccfd013dcddfaf081d176c3b427e
Andrei Hramyka <17539035+AndrewGromyko@users.noreply.github.com>
2023-01-11 17:57:40 +0100

    Added MediatR library and data mapping

---
+++ MyNotes.Application/Notes/Queries/GetNoteList/NoteListVm.cs
@@ -0,0 +1,7 @@
+﻿namespace MyNotes.Application.Notes.Queries.GetNoteList
+{
+    public class NoteListVm
+    {
+        public IList<NoteLookupDto> Notes { get; set; }
+    }
+}
---
+++ MyNotes.Application/Notes/Queries/GetNoteList/NoteLookupDto.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MyNotes.Application.Common.Mappings;
+using MyNotes.Domain;
+
+namespace MyNotes.Application.Notes.Queries.GetNoteList
+{
+    public class NoteLookupDto : IMapWith<Note>
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Note, NoteLookupDto>()
+                .ForMember(noteDto => noteDto.Id,
+                opt => opt.MapFrom(note => note.Id))
+                .ForMember(noteDto => noteDto.Title,
+                opt => opt.MapFrom(note => note.Title));
+        }
+    }
+}
