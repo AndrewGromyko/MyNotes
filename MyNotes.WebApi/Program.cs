c1b463a2976f62bda10f91694a3632bd194ace57
Andrei Hramyka <17539035+AndrewGromyko@users.noreply.github.com>
2023-01-12 10:24:09 +0100

    Updated WebAPI project, added database

--- MyNotes.WebApi/Program.cs
+++ MyNotes.WebApi/Program.cs
@@ -1,6 +1,50 @@
+using MyNotes.Persistence;
+using MyNotes.Application;
+using MyNotes.Application.Common.Mappings;
+using MyNotes.Application.Interfaces;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplication();
+builder.Services.AddPersistence(builder.Configuration);
+
+using (var scope = builder.Services.BuildServiceProvider().CreateScope()) // invoke method of Db initialization
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var context = serviceProvider.GetRequiredService<NotesDbContext>(); // for accessing dependencies
+        DbInitializer.Initialize(context); // initialize database
+    }
+    catch (Exception exception)
+    {
+
+    }
+}
+
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    config.AddProfile(new AssemblyMappingProfile(typeof(INotesDbContext).Assembly));
+});
+
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+app.UseCors("AllowAll");
+app.MapControllers();
 
 app.Run();
